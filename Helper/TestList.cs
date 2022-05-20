4e633e0cf57ee779608d6d3e22382cf1158558b9
Long <break0344@gmail.com>
2022-05-20 19:17:07 +0800

    加入專案檔案。

---
+++ Helper/TestList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCICD_webAPI.Helper
+{
+    public static class TestList
+    {
+        public static  List<int> GenerateList()
+        {
+            return new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+    }
+}
